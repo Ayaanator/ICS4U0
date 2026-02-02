79f3b3fa348e48f2e053e028822bef28dfa3b1e2
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-02-01 22:38:44 -0500

    Finish intro_2

--- First.java
+++ First.java
@@ -1,5 +1,8 @@
 public class First {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.out.println(String.format("Students\t %6d", 876));
+    System.out.println(String.format("Teachers\t %6d", 23));
+    System.out.println(String.format("Administrators\t %6d", 3));
+    System.out.println(String.format("%s %d", "Area:", 45));
+  }
 }
---
+++ Unit_1/MyFirstOwnWindow.java
@@ -0,0 +1,8 @@
+import hsa.*;
+
+public class MyFirstOwnWindow {
+  public static void main(String[] args) {
+    Console c = new Console();
+    c.println("Hello");
+  }
+}
