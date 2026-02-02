68ba019d991f315536104f21ccd8f623a0d4e8c2
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-02-02 09:40:37 -0500

    more exercise 9

--- Unit_1/Exercise9.java
+++ Unit_1/Exercise9.java
@@ -10,5 +10,17 @@ public class Exercise9 {
     
     System.out.println("-----------------------------------------------");
     System.out.println(String.format("%47s", "Class average 74.08%"));
+
+    /*System.out.println("12345678901234567890123456789012345678901234567");
+    System.out.println(String.format("%s %12s %7s %7s %8s", "Student #", "Quiz 1", "Quiz 2", "Quiz 3", "Average"));
+    
+    System.out.println(String.format("%s %11d %s %7d %s %7d %s %8.2f %s", 
+    "323144324", 67, "%", 70, "%", 78, "%", 71.67, "%"));
+    System.out.println(String.format("%s %12s %7s %7s %8s", "324435007", "89%", "73%", "86%", "82.67%"));
+    System.out.println(String.format("%s %12s %7s %7s %8s", "375842516", "67%", "64%", "58%", "63.00%"));
+    System.out.println(String.format("%s %12s %7s %7s %8s", "300644535", "95%", "67%", "75%", "79.00%"));
+    
+    System.out.println("-----------------------------------------------");
+    System.out.println(String.format("%47s", "Class average 74.08%"));*/
   }
 }
