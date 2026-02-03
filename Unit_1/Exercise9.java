44ac99894e7bfdc2b6e4d60b7c79d39efa52afed
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-02-02 20:04:34 -0500

    solve exercise 9 fully

--- Unit_1/Exercise9.java
+++ Unit_1/Exercise9.java
@@ -11,16 +11,15 @@ public class Exercise9 {
     System.out.println("-----------------------------------------------");
     System.out.println(String.format("%47s", "Class average 74.08%"));
 
-    /*System.out.println("12345678901234567890123456789012345678901234567");
+    System.out.println("\n\n12345678901234567890123456789012345678901234567");
     System.out.println(String.format("%s %12s %7s %7s %8s", "Student #", "Quiz 1", "Quiz 2", "Quiz 3", "Average"));
     
-    System.out.println(String.format("%s %11d %s %7d %s %7d %s %8.2f %s", 
-    "323144324", 67, "%", 70, "%", 78, "%", 71.67, "%"));
-    System.out.println(String.format("%s %12s %7s %7s %8s", "324435007", "89%", "73%", "86%", "82.67%"));
-    System.out.println(String.format("%s %12s %7s %7s %8s", "375842516", "67%", "64%", "58%", "63.00%"));
-    System.out.println(String.format("%s %12s %7s %7s %8s", "300644535", "95%", "67%", "75%", "79.00%"));
+    System.out.println(String.format("%s %11d%% %6d%% %6d%% %7.2f%%", "323144324", 67, 70, 78, 71.67));
+    System.out.println(String.format("%s %11d%% %6d%% %6d%% %7.2f%%", "324435007", 89, 73, 86, 82.67));
+    System.out.println(String.format("%s %11d%% %6d%% %6d%% %7.2f%%", "375842516", 67, 64, 58, 63.00));
+    System.out.println(String.format("%s %11d%% %6d%% %6d%% %7.2f%%", "300644535", 95, 67, 75, 79.00));
     
     System.out.println("-----------------------------------------------");
-    System.out.println(String.format("%47s", "Class average 74.08%"));*/
+    System.out.println(String.format("%40s %4.2f%%", "Class average", 74.08));
   }
 }
