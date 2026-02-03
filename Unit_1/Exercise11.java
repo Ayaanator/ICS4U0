7866bb673cf65adeb95a01b25baa096cc0f71b49
Ayaan Adrito <74221075+Ayaanator@users.noreply.github.com>
2026-02-03 15:08:31 +0000

    stuff

--- Unit_1/Exercise11.java
+++ Unit_1/Exercise11.java
@@ -7,7 +7,7 @@ public class Exercise11 {
     double recl = 10.3;
     double recw = 3;
     System.out.println(String.format("%s %.2f%s %.2f%s", "A rectangle has a length of", 
-    recl, "m, and a height of ", recw, "m"));
-    
+    recl, "m, and a height of", recw, "m"));
+    System.out.println(String.format("%s %.2f%s", "Therefore, its perimeter is ", recw * 2 + recl * 2, "m"));
   }
 }
