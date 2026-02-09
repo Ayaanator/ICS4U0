74d12ad4750655b42e87b71d626afdda22c8c681
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-02-09 10:02:59 -0500

    print window

--- First.java
+++
@@ -1,8 +0,0 @@
-public class First {
-  public static void main(String[] args) {
-    System.out.println(String.format("Students\t %6d", 876));
-    System.out.println(String.format("Teachers\t %6d", 23));
-    System.out.println(String.format("Administrators\t %6d", 3));
-    System.out.println(String.format("%s %d", "Area:", 45));
-  }
-}
---
+++ RectangleExample.java
@@ -0,0 +1,13 @@
+import hsa.Console;
+
+public class RectangleExample {
+  static Console c;
+
+  public static void main(String[] args) {
+    c = new Console();
+    
+
+    c.drawRect(50, 50, 100, 60);
+
+  }
+}
