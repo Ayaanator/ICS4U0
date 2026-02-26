e730fada67e75b8abb438a2fb51db7725e90dabb
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-02-25 20:25:44 -0500

    solve GuessingGame correctly

--- 3/GuessingGame.java
+++ 3/GuessingGame.java
@@ -8,7 +8,7 @@ public class GuessingGame {
     int mx = c.maxx();
     int my = c.maxy();
 
-    int max_x = 100;
+    int max_x = mx + 1;
 
     int numToGuess = (int)(Math.random() * max_x);
     
@@ -41,44 +41,32 @@ public class GuessingGame {
     c.drawLine(70, 40, 70, 110);
 
     while(guess != numToGuess) {
-      c.print("Enter number to guess: ");
+      c.print("Enter number to guess: (" + numToGuess + ")");
       guess = c.readInt();
       tries++;
       int ggx = 0;
 
       if(guess < numToGuess) {
         c.print("Your guess is too low. Try a larger number.\n");
-
-        double pp = (double)guess/(double)max_x;
-        ggx = (int)((pp * (mx + 1)) - 1);
-
-        // ggx = (int)Math.round(((double)mx * (double)guess/(double)max_x));
-
-        c.drawLine(ggx, ly - 25, ggx, ly + 25);
       } else if(guess > numToGuess) {
         c.print("Your guess is too high. Try a smaller number.\n");
-
-        double pp = (double)guess/(double)max_x;
-        ggx = (int)((pp * (mx + 1)) - 1);
-
-        //ggx = (int)Math.round(((double)mx * (double)guess/(double)max_x));
-        c.drawLine(ggx, ly - 25, ggx, ly + 25);
       }
 
       c.setColor(Color.blue);
-      c.print(gx + "\n");
-      c.print(ggx + "\n");
 
       if(guess == numToGuess) {
         // ggx = (int)Math.round(((double)mx * (double)guess/(double)max_x));
-
-        double pp = (double)guess/(double)max_x;
-        ggx = (int)((pp * (mx + 1)) - 1);
-
         c.drawLine(gx, ly - 25, gx, ly + 25);
         c.print("You guessed in " + tries + (tries > 1 ?" tries." : " try."));
-        c.print("guess: " + guess + " numGuess: "  + numToGuess); 
+        // c.print("guess: " + guess + " numGuess: "  + numToGuess); 
       }
+
+      double pp = (double)guess/(double)max_x;
+      ggx = (int)((pp * (mx + 1)) - 1);
+      c.drawLine(ggx, ly - 25, ggx, ly + 25);
+
+      // c.print(gx + "\n");
+      // c.print(ggx + "\n");
     }
   }
 }
