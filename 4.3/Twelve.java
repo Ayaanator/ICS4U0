5c4cd2308a8399e9a67356fefd0d31f88fd37b2c
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-03-10 21:44:33 -0400

    merge

--- 4.3/Twelve.java
+++ 4.3/Twelve.java
@@ -7,15 +7,9 @@ public class Twelve {
 
     //System.out.print("Enter thing: ");
     //String userInput = input.nextLine();
-    String userInput = "PIZZA PLEASE";
-    String pswd = "AB:1AB:1AB:1";
-    String cipherInput = "";
-    
-    for(int i = 0; i < userInput.length(); i++) {
-      //cipherInput += i + 1 % 2 == 0 ? (char)((int)userInput.charAt(i) - 1) : (char)((int)userInput.charAt(i) + 1);
-      cipherInput += (char)((int)pswd.charAt(i) + (int)userInput.charAt(i) - 30);
-    }
+    String labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String rotor1 = "OKDEMFZGXHCTNPQRASLUVWJBYI";
 
-    System.out.println(cipherInput);
+    
   }
 }
